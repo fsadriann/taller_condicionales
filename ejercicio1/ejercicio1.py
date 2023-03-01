d94cc6da07909bdb1d407325d4a248bc3c331d30
sistemas <adrianforero@colegioguanenta.edu.co>
2023-03-01 09:16:04 -0500

    se añadió el ejercicio 1

---
+++ ejercicio1/ejercicio1.py
@@ -0,0 +1,13 @@
+print("----------------------------------------------")
+print("---------- Digite el valor de x, y -----------")
+print("----------------------------------------------")
+
+#input
+
+x = int(input("Digite el valor de x: "))
+y = int(input("Digite el valor de y: "))
+
+if x==0 and y==0:
+    msj = "las coordenadas están en el origen"
+else:
+    
